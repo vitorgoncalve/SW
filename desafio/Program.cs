55f9f9ccf8184b58110590093519a9994ab40d64
Vitor Antonio <100379832+vitorgoncalve@users.noreply.github.com>
2023-03-15 16:37:54 -0300

    Add files via upload

---
+++ desafio/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace exerciciosAlunos;
+
+
+class program 
+{
+     static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+
+        Console.WriteLine(" nome do aluno: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("O nome é: " + nome);
+
+
+
+        Console.WriteLine("primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A primeira nota é: " + nota1);
+
+
+
+        Console.WriteLine("segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A segunda nota é: " + nota2);
+
+
+        aluno1.nome = nome;
+        aluno1.nota1 = nota1;
+        aluno1.nota2 = nota2;
+
+        aluno1.mensagem();
+         }
+}
