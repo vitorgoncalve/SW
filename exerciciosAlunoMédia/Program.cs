b5603d27ed88d8a606ad29d720a89de4fffef9c1
Vitor Antonio <100379832+vitorgoncalve@users.noreply.github.com>
2023-03-15 15:11:43 -0300

    Add files via upload

---
+++ exerciciosAlunoMédia/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exerciciosAlunos;
+
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Vitor";
+        aluno1.nota1 = 4.3;
+        aluno1.nota2 = 4.8;
+        aluno1.mensagem();
+    }
+}
+
